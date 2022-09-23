9fa4f5967c38c6bbfe29e2407eaf28ae355a3f76
Ramazan USLU <ruslu1720@gmail.com>
2022-09-24 02:10:20 +0300

    react router dom v-6 eklendi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        {/* <Route path="/" element={} /> */}
+      </Routes>
     </>
   );
 }
