44154f1df89344e25e10b5ea74362ee67dd433d8
Ramazan USLU <ruslu1720@gmail.com>
2022-09-25 02:42:59 +0300

    Anasyafa - Kategorilerin yer aldıgi sayfa hazirlandı

---
+++ src/components/CategoryCard/CategoryCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import dummy from "../../image/dummy.jpg";
+
+function CategoryCard({ item }) {
+  return (
+    <>
+      <div className="col-sm-6 col-lg-4 col-xl-3">
+        <Link to={`/category/${item.ID}`}>
+          <div className="card mt-5">
+            <div className="card-head">
+              <img
+                src={item.ImageUri === "" ? dummy : item.ImageUri}
+                className="card-img-top"
+                alt={item.Code}
+              />
+            </div>
+          </div>
+          <div className="card-text card-title my-3 text-center">
+            {item.DisplayName}
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default CategoryCard;
--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,10 +1,29 @@
 import React from "react";
+import Loading from "../../components/Loading/Loading";
+import { useQuery } from "react-query";
+import { fetchCategoryList } from "../../api";
+import CategoryCard from "../../components/CategoryCard/CategoryCard";
 
 function Categories() {
+  const { isLoading, error, data } = useQuery("category", fetchCategoryList);
+
+  if (isLoading) return <Loading />;
+
+  if (error) return "An error has occurred: " + error.message;
+
+  console.log(data);
+  console.log(data.Result.TreeList);
   return (
-    <div>
-      <h1>Category Page</h1>
-    </div>
+    <>
+      <div className="container my-5">
+        <div className="row">
+          {data.Result.TreeList.map(
+            (item, key) =>
+              item.ID < 11 && <CategoryCard key={key} item={item} />
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
