27cdba38c12250534cfe2333d33b4c32df417572
Ramazan USLU <ruslu1720@gmail.com>
2022-09-24 03:32:44 +0300

    axios eklendi

--- src/components/Navbar/DesktopNav/DesktopNav.js
+++ src/components/Navbar/DesktopNav/DesktopNav.js
@@ -3,21 +3,19 @@ import React from "react";
 function DesktopNav() {
   return (
     <>
-      <div>
-        <nav className="bg-danger d-none d-xl-block">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <ul className="nav justify-content-center">
-                  <li className="nav-item menu-item">
-                    <span className="nav-link menu-link">Tv - Ses</span>
-                  </li>
-                </ul>
-              </div>
+      <nav className="bg-danger d-none d-xl-block">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <ul className="nav justify-content-center">
+                <li className="nav-item menu-item">
+                  <span className="nav-link menu-link">Tv - Ses</span>
+                </li>
+              </ul>
             </div>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </>
   );
 }
