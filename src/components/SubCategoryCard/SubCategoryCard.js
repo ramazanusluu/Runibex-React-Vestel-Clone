5254c859c061dafa3348c37b64575c11582e7320
Ramazan USLU <ruslu1720@gmail.com>
2022-09-25 10:51:56 +0300

    SubCategoryCard component'i tamamlanarak subCatgory sayfasi olusturuldu & Mobil Navbar icin yeni css ozellikleri eklendi

--- src/components/SubCategoryCard/SubCategoryCard.js
+++ src/components/SubCategoryCard/SubCategoryCard.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import dummy from "../../image/dummy.jpg";
 
-function SubCategoryCard() {
+function SubCategoryCard({ item }) {
   return (
-    <div>
-      <h1>Sub Category Card</h1>
-    </div>
+    <>
+      <div className="col-sm-6 col-lg-4 col-xl-3">
+        <Link to={`/products/${item.ID}`}>
+          <div className="card subCategory-card mt-5">
+            <div className="card-head subCategory-card-head">
+              <img
+                src={!item.ImageUri ? dummy : item.ImageUri}
+                className="card-img-top"
+                alt={item.DisplayName}
+              />
+            </div>
+          </div>
+          <div className="card-text card-title my-3 text-center">
+            {item.DisplayName}
+          </div>
+        </Link>
+      </div>
+    </>
   );
 }
 
