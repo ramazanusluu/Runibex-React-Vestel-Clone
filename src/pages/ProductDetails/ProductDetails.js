02fdecc7c771024e559bca413127cfa0e63d6200
Ramazan USLU <ruslu1720@gmail.com>
2022-09-27 10:37:26 +0300

    product Detail icin gerekli olan url'e istek atıldı

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import Loading from "../../components/Loading/Loading";
+import { fetchProductDetail } from "../../api";
+import ProductDetail from "../../components/ProductDetail/ProductDetail";
 
 function ProductDetails() {
+  const { product } = useParams();
+  const { isLoading, error, data } = useQuery(["productDetail", product], () =>
+    fetchProductDetail(product)
+  );
+
+  if (isLoading) return <Loading />;
+
+  if (error) return "An error has occurred: " + error.message;
+
+  console.log(data);
   return (
     <div>
-      <h1>Product Details Page Component</h1>
+      <ProductDetail data={data} />
     </div>
   );
 }
