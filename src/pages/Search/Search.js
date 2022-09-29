19fb9f53cb1820bfc9365559c3742734faac5d06
Ramazan USLU <ruslu1720@gmail.com>
2022-09-30 02:09:12 +0300

    search input alani olusturuldu

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 
 function Search() {
+  const [search, setSearch] = useState("");
   return (
-    <div>
-      <h1> Search Component</h1>
-    </div>
+    <>
+      <Helmet>
+        <title>Arama Sayfası | Vestel</title>
+        <meta name="description" content="vestel" />
+      </Helmet>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 mx-auto my-3">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                className="form-control search-input"
+                placeholder="Ürün, kategori, servis, mağaza ara"
+              />
+              <button
+                className="btn search-button"
+                type="button"
+              >
+                Ara
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
