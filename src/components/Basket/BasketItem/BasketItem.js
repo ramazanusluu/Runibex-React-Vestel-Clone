293478ab91bf1894a099ed64910adde89c396162
Ramazan USLU <ruslu1720@gmail.com>
2022-10-02 14:37:09 +0300

    Alışveris sepetindeki bilgilendirme kismi tamamlandi

---
+++ src/components/Basket/BasketItem/BasketItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BasketItem() {
+  return (
+    <>
+      <h4 className="basket-item-title">SEPETİM</h4>
+      <h5 className="basket-item-info">
+        Sepetiniz hazır ise <span>Ödeme Sayfasına Devam Et</span> butonuna
+        tıklayarak işleminizi tamamlayabilirsiniz.
+      </h5>
+    </>
+  );
+}
+
+export default BasketItem;
--- src/pages/ShoppingCard/ShoppingCard.js
+++ src/pages/ShoppingCard/ShoppingCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import BasketItem from "../../components/Basket/BasketItem/BasketItem";
 import EmptyBasket from "../../components/Basket/EmptyBasket/EmptyBasket";
 
 function ShoppingCard() {
@@ -7,9 +8,12 @@ function ShoppingCard() {
   return (
     <>
       <div className="card-header"></div>
-      <div className="container my-5">
+      <div className="container my-4">
         <div className="row">
           {card.cardItems.length < 1 && <EmptyBasket />}
+          <div className="col-md-8">
+            {card.cardItems.length > 0 && <BasketItem />}
+          </div>
         </div>
       </div>
     </>
