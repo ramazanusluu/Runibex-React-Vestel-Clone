5a671ccd8b07333d92b8c641a3d54a6eecbb8eea
Ramazan USLU <ruslu1720@gmail.com>
2022-10-02 15:14:57 +0300

    Sepette bulunan uzrunleri listelemek icin temel sablon olusturuldu

--- src/components/Basket/BasketItem/BasketItem.js
+++ src/components/Basket/BasketItem/BasketItem.js
@@ -8,6 +8,24 @@ function BasketItem() {
         Sepetiniz hazır ise <span>Ödeme Sayfasına Devam Et</span> butonuna
         tıklayarak işleminizi tamamlayabilirsiniz.
       </h5>
+      <div className="row mt-5 mb-4">
+        <div className="col-lg-7 d-none d-lg-block basket-table-title">
+          Sepetteki Ürünlerim
+        </div>
+        <div className="col-lg-2 text-center d-none d-lg-block basket-table-title">
+          Adet
+        </div>
+        <div className="col-lg-2 text-center d-none d-lg-block basket-table-title">
+          Toplam
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-3 text-center">Resim</div>
+        <div className="col-lg-4 text-center">Bilgi</div>
+        <div className="col-lg-2 text-center">Adet</div>
+        <div className="col-lg-2 text-center">Toplam</div>
+        <div className="col-lg-1 text-center">Temizle</div>
+      </div>
     </>
   );
 }
